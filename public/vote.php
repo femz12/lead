65d2d41fd64d3c3b817cf5dd25dea8f7fbe86a3b
femz12 <femz12@yahoo.com>
2018-04-26 12:05:13 +0100

    added vote.php file

---
+++ public/vote.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Meta Data -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>About Us - LEAD Project | 2018</title>
+    <meta name="description" content="A Lead project by bullion"/>
+    <meta name="keywords" content="About LEAD, "/>
+    <meta name="author" content="Bullion Projects"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+    <!-- Faviconss -->
+    <link rel="shortcut icon" href="images/favicon-16x16.png">
+
+    <!-- Stlylesheet -->
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+
+    <!-- Skin Color -->
+    <link rel="stylesheet" href="css/colors/red.css" id="color-skins"/>
+
+</head>
+<body>
+
+<!-- Start Preloader -->
+<div id="preloader">
+    <div class="loader">
+        <img src="img/assets/spinner.gif" alt="LEAD Project">
+    </div>
+</div>
+<!-- End Preloader -->
+
+<!-- Start Header -->
+<header>
+    <nav class="navbar navbar-default navbar-alt" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand to-top" rel="home" href="index.php">
+                    <!--<img src="img/assets/lead-logo.png" alt="LEAD Project" class="logo-big">
+                    <img src="img/assets/logo-white.png" alt="" class="logo-big">-->
+                    <img src="img/assets/lead-logo-black.png" alt="LEAD Project" class="logo-small">
+                </a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="main-nav">
+                <ul class="nav navbar-nav  navbar-right">
+
+                    <li class="to-section"><a href="/">Home</a></li>
+                    <li class="to-section"><a href="#">About Us</a></li>
+                    <li class="to-section"><a href="index.php#speakers">Speakers</a></li>
+                    <li class="to-section"><a href="gallery.php">Gallery</a></li>
+
+                    <!--<li class="dropdown">
+                        <span class="dropdown-toggle" data-toggle="dropdown">Pages <b class="caret"></b></span>
+                        <ul class="dropdown-menu" role="menu">
+                            <li class="active"><a href="#">Something Here</a></li>
+
+                        </ul>
+                    </li>
+
+                    <li class="dropdown">
+                        <span class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></span>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Something Here</a></li>
+                            <li><a href="#">Something Here</a></li>
+
+                        </ul>
+                    </li> -->
+                    <li class="to-section"><a href="vote.php">Vote</a></li>
+                    <li class="to-section"><a href="index.php#contact">Contact Us</a></li>
+                    <li><a class="social-icon" href="#"><i class="icon icon-social-twitter"></i></a></li>
+                    <li><a class="social-icon" href="https://www.facebook.com/TheLEADProjectAfrica/"><i class="icon icon-social-facebook"></i></a></li>
+                    <li><a class="social-icon" href="#"><i class="icon icon-support"></i></a></li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container -->
+    </nav>
+</header>
+
+<!-- Start Page Top -->
+<section id="page-top" style="height:350px">
+    <div class="hero">
+
+        <img src="img/backgrounds/bg-3.jpg" alt="LEAD Project">
+
+        <div class="page-top-title text-center">
+            <h2 class="white op-1">VOTE</h2>
+            <p class="home-subheading op-1"></p>
+        </div>
+
+    </div>
+</section>
+<!-- End Page Top -->
+
+<div class="site-wrapper content">
+
+    <!-- Start Blog Classic -->
+    <section id="blog">
+        <div class="container">
+            <div class="row">
+
+                <!-- Blog Post -->
+                <div class="col-md-8 col-md-offset-2">
+
+                    <div class="blog-post">
+
+                        <p class="lead">Not available yet</p>
+                    </div>
+                </div>
+    </section>
+    <!-- End Blog Classic -->
+
+    <!-- Start Footer 1 -->
+    <footer id="footer">
+        <div class="footer-widgets">
+            <div class="container">
+                <!-- -->
+
+                <div class="col-md-3 col-sm-6 col-footer">
+                    <h4>About Bullion</h4>
+                    <p>Bullion Projects, the official consultant is one of the West Africa’s Leaders in ideas, TV contents and media project development she consults for organisations and partners with governments on enterprise and development projects.</p>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-footer">
+                    <h4>Projects</h4>
+                    <p>She has partnered in execution of mega TV projects like the Dialogue of Leaders TV series, The NET TITAN Reality TV series and pioneered world class events like PAN African Enterpreneurship summit, Nigerian Job Forum, Lead Africa, etc.</p>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-footer">
+                    <div class="contact">
+                        <h4>Visit us Today!</h4>
+                        <ul class="contact-details">
+                            <li><p><i class="icon ion-ios-location-outline highlight"></i> 21, Ezekiel Street, Off Toyin, Ikeja</p></li>
+                            <li><p><i class="icon ion-ios-telephone highlight"></i>+2348170475049 or 08143497350</p></li>
+                            <li><p><i class="icon ion-android-mail highlight"></i> <a href="leadbiginnovationfestival@gmail.com">leadbiginnovationfestival@gmail.com</a></p></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6 col-twitter">
+                    <img src="img/assets/bullion-logo.jpg" alt="LEAD Project" class="logo-big">
+                </div>
+
+            </div>
+        </div><!-- End Footer Widgets -->
+
+        <div class="footer-copyright">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 col-sm-12">
+                        <p>© 2018 Lead Project Africa, All Rights Reserved. </p>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <ul id="social-icons">
+                            <li><a href="#"><i class="icon ion-social-twitter"></i></a></li>
+                            <li><a href="https://www.facebook.com/TheLEADProjectAfrica/"><i class="icon ion-social-facebook"></i></a></li>
+                            <!--<li><a href="#"><i class="icon ion-social-googleplus"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-instagram-outline"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-pinterest"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-skype"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-linkedin"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-dropbox"></i></a></li>
+                            <li><a href="#"><i class="icon ion-social-vimeo"></i></a></li>-->
+                            <li><a href="#"><i class="icon ion-social-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Footer Copyright -->
+
+    </footer>
+    <!-- End Footer 1 -->
+
+    <!-- Start Back To Top -->
+    <a id="back-to-top"><i class="icon ion-chevron-up"></i></a>
+    <!-- End Back To Top -->
+</div><!-- End Site Wrapper -->
+<!-- jQuery -->
+<script src="js/plugins/jquery.min.js"></script>
+<script src="js/plugins/smoothscroll.min.js"></script>
+<script src="js/plugins/revslider.min.js"></script>
+<script src="js/plugins/bootstrap.min.js"></script>
+<script src="js/plugins/waypoints.min.js"></script>
+<script src="js/plugins/parallax.min.js"></script>
+<script src="js/plugins/easign1.3.min.js"></script>
+<script src="js/plugins/moderniz.min.js"></script>
+<script src="js/plugins/cubeportfolio.min.js"></script>
+<script src="js/plugins/owlcarousel.min.js"></script>
+<script src="js/plugins/tweetie.min.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script src="js/plugins/gmap3.min.js"></script>
+<script src="js/plugins/wow.min.js"></script>
+<script src="js/plugins/counterup.min.js"></script>
+<script src="js/scripts.js"></script>
+</body>
+</html>
